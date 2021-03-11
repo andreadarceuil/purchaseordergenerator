e887e302da91ce50315fff6b9e71f618298f5be4
Andrea Darceuil <80012899+andreadarceuil@users.noreply.github.com>
2021-03-10 22:43:03 -0500

    client side

---
+++ app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { VendorHomeComponent } from './vendor/vendor-home.component';
+import { ProductHomeComponent } from './product/product-home.component';
+import { PoGeneratorComponent } from './po/generator/po-generator.component';
+import {PoViewerComponent} from './po/viewer/po-viewer.component';
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'vendors', component: VendorHomeComponent },
+  { path: 'products', component: ProductHomeComponent },
+  { path: 'generator', component: PoGeneratorComponent },
+  { path: 'viewer', component: PoViewerComponent },
+  { path: '', component: HomeComponent}
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
